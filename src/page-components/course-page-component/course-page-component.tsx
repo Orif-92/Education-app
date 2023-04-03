b83a96a254e515645ec64e3266574bc7b3e996a1
Orif-92 <sarniyozovorif@gmail.com>
2023-04-03 20:10:28 +0300

    Product animation

--- src/page-components/course-page-component/course-page-component.tsx
+++ src/page-components/course-page-component/course-page-component.tsx
@@ -1,18 +1,47 @@
 import { CoursePageComponentProps } from './course-page-component.props';
 import styles from './course-page-component.module.css';
 import { Advantages, Heading, HhData, Product, Sort, Tag, Text } from '../../components';
+import { useEffect, useReducer } from 'react';
+import { sortReducer } from './sort.reducer';
+import { SortEnum } from '../../components/sort/sort.props';
+import { AnimatePresence } from 'framer-motion';
 
 const CoursePageComponent = ({ page, products }: CoursePageComponentProps): JSX.Element => {
+	const [state, dispatch] = useReducer(sortReducer, { sort: SortEnum.Rating, products: products });
+
+	const setSort = (sort: SortEnum) => {
+		dispatch({ type: sort });
+	};
+
+	useEffect(() => {
+		dispatch({ type: 'reset', initialState: products });
+	}, [products]);
+
+	const spring = {
+		type: 'spring',
+		stiffness: 500,
+		damping: 10,
+	};
+
+	const animations = {
+		initial: { scale: 0 },
+		animate: { scale: 1 },
+		exit: { scale: 1 },
+	};
+
 	return (
 		<div className={styles.wrapper}>
 			{/* TITLE */}
 			<div className={styles.title}>
 				<Heading tag='h1'>{page.title}</Heading>
-				<Sort />
+				<Sort sort={state.sort} setSort={setSort} />
 			</div>
 
 			{/* PRODUCTS */}
-			<div>{products && products.map((c, idx) => <Product key={idx} product={c} />)}</div>
+			<AnimatePresence>
+				{state.products &&
+					state.products.map((c, idx) => <Product key={idx} layout transition={spring} {...animations} product={c} />)}
+			</AnimatePresence>
 
 			{/* VACATIONS */}
 			<div className={styles.hhTitle}>
