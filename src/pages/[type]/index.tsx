c7e284e18721f3569fa26e2fd4578ff6f950caf5
Orif-92 <sarniyozovorif@gmail.com>
2023-04-05 01:09:25 +0300

    Home page component

--- src/pages/[type]/index.tsx
+++ src/pages/[type]/index.tsx
@@ -1,42 +1,7 @@
-import { GetServerSideProps } from 'next';
-import React from 'react';
-import { firstLevelMenu } from '../../helpers/constants';
-import axios from 'axios';
-import { MenuItem } from '../../interfaces/menu.interface';
-import { withLayout } from '../../layout/layout';
+import { HomePageComponent } from '../../page-components';
 
-const Type = () => {
-	return <div>Type</div>;
+const Index = (): JSX.Element => {
+	return <HomePageComponent />;
 };
 
-export default withLayout(Type);
-
-export const getServerSideProps: GetServerSideProps<TypeProps> = async ({ query }) => {
-	const { type } = query;
-
-	if (!type) {
-		return { notFound: true };
-	}
-
-	const firstCategoryItem = firstLevelMenu.find(m => m.route === type);
-
-	if (!firstCategoryItem) {
-		return { notFound: true };
-	}
-
-	const { data: menu } = await axios.post<MenuItem[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/api/page-find`, {
-		firstCategory: firstCategoryItem.id,
-	});
-
-	return {
-		props: {
-			menu,
-			firstCategory: firstCategoryItem.id,
-		},
-	};
-};
-
-interface TypeProps extends Record<string, unknown> {
-	menu: MenuItem[];
-	firstCategory: number;
-}
+export default Index;
