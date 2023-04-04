70ca9ec00c8de09964a1089c6742889ae2eb7587
Orif-92 <sarniyozovorif@gmail.com>
2023-04-04 23:55:27 +0300

    Storybook compoennts

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import orifTheme from './orif-theme';
+
+addons.setConfig({
+	theme: orifTheme,
+});
---
+++ .storybook/orif-theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+	base: 'light',
+	brandTitle: 'Orif',
+	brandUrl: 'https://sammi.ac',
+	brandImage: 'https://cdn.dribbble.com/users/33271/screenshots/3975282/media/e803cde680942ba882d6e8db8cb79071.gif',
+	brandTarget: '_blank',
+});
